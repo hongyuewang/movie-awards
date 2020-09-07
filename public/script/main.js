820f0177da6eb85f0e66a0a01ed4a7a481798e5d
Hong Yue Wang <hongyuew26@gmail.com>
2020-09-06 20:18:37 -0400

    Add banner page

--- public/script/main.js
+++ public/script/main.js
@@ -1,10 +1,11 @@
 nominationList = [];
+searchText = '';
 
 $(document).ready( () => {
     $('#searchForm').on('keyup', (e) => {
         document.getElementById("movies-header").innerHTML = "Results";
         document.getElementById("nominations-header").innerHTML = "Nominations";
-        let searchText = $('#searchText').val();
+        searchText = $('#searchText').val();
         getMovies(searchText);
         e.preventDefault();
     });
@@ -18,15 +19,26 @@ function getMovies(searchText) {
         let movies = response.data.Search;
         let output = '';
         $.each(movies, (index, movie) => {
-            output += `
-            <div class="col-md-3">
-                <div class="well text-center">
-                    <img src="${movie.Poster}">
-                    <h5 class="movie-title">${movie.Title}</h5>
-                    <a onclick="nominate('${movie.imdbID}')" class= "btn btn-primary btn-block" href="#">Nominate</a>
+            if (!nominationList.includes(movie.imdbID)) {
+                output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="${movie.Poster}">
+                        <h5 class="movie-title">${movie.Title} (${movie.Year})</h5>
+                        <a onclick="nominate('${movie.imdbID}')" class= "btn btn-primary btn-block" href="#">Nominate</a>
+                    </div>
                 </div>
-            </div>
-            `;
+                `;
+            } else {
+                output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                        <img src="${movie.Poster}">
+                        <h5 class="movie-title">${movie.Title} (${movie.Year})</h5>
+                    </div>
+                </div>
+                `;
+            }
         });
 
         $('#movies').html(output);
@@ -39,6 +51,7 @@ function getMovies(searchText) {
 function nominate(movieID) {
     nominationList.push(movieID);
     getNominations(nominationList);
+    getMovies(searchText);
 }
 
 function getNominations(list) {
@@ -52,7 +65,7 @@ function getNominations(list) {
                 <div class="col-md-3">
                     <div class="well text-center">
                         <img src="${movie.Poster}">
-                        <h5 class="movie-title">${movie.Title}</h5>
+                        <h5 class="movie-title">${movie.Title} (${movie.Year})</h5>
                         <a onclick="remove('${movie.imdbID}')" class= "btn btn-primary btn-block" href="#">Remove</a>
                     </div>
                 </div>
@@ -65,6 +78,7 @@ function getNominations(list) {
         $('#nominations').empty();
     } else if (list.length == 5) {
         // Create banner
+        location.replace("../banner.html");
 
     } else {
         return;
@@ -77,4 +91,5 @@ function remove(movieID) {
     nominationList.splice(pos, 1);
     console.log(nominationList.length);
     getNominations(nominationList);
+    getMovies(searchText);
 }
